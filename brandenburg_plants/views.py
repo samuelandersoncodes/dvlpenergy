8e63919f8e0f12e2180297b3563267fcfdfcac17
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 23:29:46 +0200

    feat: Create view to serve react app

--- brandenburg_plants/views.py
+++ brandenburg_plants/views.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
 from .models import SolarPlant
 from .serializers import SolarPlantSerializer
+from django.shortcuts import render
 
 
 class SolarPlantViewSet(viewsets.ModelViewSet):
     """
     Viewset for viewing and editing SolarPlant instances.
-    It provides CRUD actions for solar plant objects. 
+    It provides CRUD actions for solar plant objects.
     It also uses the Solar plant serializer for serialization
     and deserialization of data.
     """
     queryset = SolarPlant.objects.all()
     serializer_class = SolarPlantSerializer
+
+
+def index(request):
+    # View to serve the react app
+    return render(request, "index.html")
