8b22a297862f7bd40a231e5c1317f6b8aa0ecca5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 20:19:01 +0200

    feat: Set mapbox gl service access token for interaction

--- frontend/map-display/src/components/SolarPlantMap.tsx
+++ frontend/map-display/src/components/SolarPlantMap.tsx
@@ -1,3 +1,5 @@
+import mapboxgl, { Map, LngLatLike } from 'mapbox-gl';
+
 
 // Load Mapbox access token from environment variables
 const ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
@@ -7,6 +9,7 @@ if (!ACCESS_TOKEN) {
     throw new Error('Mapbox access token is required');
 }
 
+mapboxgl.accessToken = ACCESS_TOKEN;
 
 const SolarPlantMap: React.FC = () => {
     return (
