bc89a02ea62e91c2af26b973e1eb3f3710923ffe
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 16:55:33 +0200

    feat: Add router and its path

--- brandenburg_plants/urls.py
+++ brandenburg_plants/urls.py
@@ -1,1 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import SolarPlantViewSet
+
+# Create a router object
+router = DefaultRouter()
+
+# Register SolarPlantViewSet with the router
+router.register(r'solar_plants', SolarPlantViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
