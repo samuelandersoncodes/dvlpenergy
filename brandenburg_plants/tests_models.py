5a096da1960284bacb6352f476e6bbe0f9bbca16
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 14:49:54 +0200

    test: Add solar plant string representation test

--- brandenburg_plants/tests_models.py
+++ brandenburg_plants/tests_models.py
@@ -13,5 +13,11 @@ class SolarPlantModelTestCase(TestCase):
             geometry='{"type": "Point", "coordinates": [1.0, 2.0]}'
         )
 
-    
+    def test_solar_plant_str(self):
+        """
+        This function tests the string representation
+        of the solar plant object
+        """
+        plant = SolarPlant.objects.get(name='Test Plant')
+        self.assertEqual(str(plant), 'Test Plant')
 
