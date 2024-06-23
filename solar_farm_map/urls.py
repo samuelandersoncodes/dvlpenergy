8bcf071f1bc729bafb737eb1766358ef5d139d21
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 16:57:39 +0200

    refactor: Modify main url for cohesion

--- solar_farm_map/urls.py
+++ solar_farm_map/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('brandenburg-plants/', include('brandenburg_plants.urls')),
+    path('', include('brandenburg_plants.urls')),
 ]
