4e61ddf9c44d5143f171dce570247c1c29e353c3
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 21:47:07 +0200

    feat: Wrap fetch in useeffect hook

--- frontend/map-display/src/components/SolarPlantMap.tsx
+++ frontend/map-display/src/components/SolarPlantMap.tsx
@@ -1,5 +1,5 @@
 import mapboxgl, { Map, LngLatLike } from 'mapbox-gl';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 
 // Load Mapbox access token from environment variables
@@ -20,34 +20,36 @@ const SolarPlantMap: React.FC = () => {
     // Creates a reference to store the Mapbox map instance
     const map = useRef<Map | null>(null);
 
-    // Fetch solar plant data from the Django API
-    fetch('/api/solar_plants/')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach((solarPlant: any) => {
-                const geometry = JSON.parse(solarPlant.geometry);
-                // Add solar plant polygon to the map
-                if (map.current) {
-                    map.current.on('load', () => {
-                        map.current!.addSource(solarPlant.id, {
-                            'type': 'geojson',
-                            'data': {
-                                'type': 'Feature',
-                                'geometry': geometry,
-                                'properties': {}
-                            }
+    useEffect(() => {
+
+        // Fetch solar plant data from the Django API
+        fetch('/api/solar_plants/')
+            .then(response => response.json())
+            .then(data => {
+                data.forEach((solarPlant: any) => {
+                    const geometry = JSON.parse(solarPlant.geometry);
+                    // Add solar plant polygon to the map
+                    if (map.current) {
+                        map.current.on('load', () => {
+                            map.current!.addSource(solarPlant.id, {
+                                'type': 'geojson',
+                                'data': {
+                                    'type': 'Feature',
+                                    'geometry': geometry,
+                                    'properties': {}
+                                }
+                            });
                         });
-                    });
-                }
+                    }
+                });
             });
-        });
 
-}, []);
+    }, []);
 
-return (
-    <>
-    </>
-)
+    return (
+        <>
+        </>
+    )
 }
 
 export default SolarPlantMap;
