4c3615b6ced68b98f64fce3e78e33b94cffbccb5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 14:45:31 +0200

    test: Write test for solar plant

--- brandenburg_plants/tests_models.py
+++ brandenburg_plants/tests_models.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import SolarPlant
 
+class SolarPlantModelTestCase(TestCase):
+    def setUp(self):
+        """
+        This function sets up a test environment
+        by creating a sample SolarPlant object
+        and a GeoJSON geometry coordinate points
+        """
+        SolarPlant.objects.create(
+            name='Test Plant', 
+            geometry='{"type": "Point", "coordinates": [1.0, 2.0]}'
+        )
+
+    
 
