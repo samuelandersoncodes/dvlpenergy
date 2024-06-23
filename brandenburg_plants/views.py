6e97e70708104d1e3b150b34c871ebd0255a3911
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-23 16:42:34 +0200

    feat: Add viewset for solar plant

--- brandenburg_plants/views.py
+++ brandenburg_plants/views.py
@@ -1,3 +1,14 @@
 from rest_framework import viewsets
 from .models import SolarPlant
 from .serializers import SolarPlantSerializer
+
+
+class SolarPlantViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for viewing and editing SolarPlant instances.
+    It provides CRUD actions for solar plant objects. 
+    It also uses the Solar plant serializer for serialization
+    and deserialization of data.
+    """
+    queryset = SolarPlant.objects.all()
+    serializer_class = SolarPlantSerializer
