d462f68c28bbefe897c9f65aaee93fc938a6c2a3
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-24 23:32:06 +0200

    feat: Fetch api endpoint

--- frontend/src/components/SolarPlantsMap.tsx
+++ frontend/src/components/SolarPlantsMap.tsx
@@ -1,4 +1,18 @@
+import React, { useEffect } from 'react';
+
 const SolarPlantMap: React.FC = () => {
+
+    useEffect(() => {
+
+        // Fetch solar plant data from the Django API
+        fetch('/api/solar_plants/')
+            .then(response => response.json())
+            .then(data => {
+                data.forEach((solarPlant: any) => {
+                    const geometry = JSON.parse(solarPlant.geometry);
+                });
+            });
+    }, []);
 
     return (
         <></>
