795dd2f303335f399fa2217e5d720f6757a3fbdf
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-24 16:20:04 +0200

    refactor: Modify template configuration

--- solar_farm_map/settings.py
+++ solar_farm_map/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "solar_farm_map.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
