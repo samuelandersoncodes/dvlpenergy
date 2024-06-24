f1e2a696190b12a1823cf28c5e2271843417f456
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-24 14:54:48 +0200

    refactor: Modify static file base dir

--- solar_farm_map/settings.py
+++ solar_farm_map/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/map-display/dist/assets')
+    os.path.join(BASE_DIR, 'frontend/map-display/dist')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
