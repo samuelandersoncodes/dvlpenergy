ac9830b93499e0ec95a0d7e154aed2d8adfa4977
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-25 15:18:52 +0200

    feat: Remove logging settings

--- solar_farm_map/settings.py
+++ solar_farm_map/settings.py
@@ -149,25 +149,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["console"],
-            "level": "DEBUG",
-            "propagate": True,
-        },
-    },
-}
