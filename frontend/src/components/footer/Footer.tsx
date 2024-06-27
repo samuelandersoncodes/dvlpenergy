e1dab8aad8915155d52b52e904e6d874636c4719
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 22:06:15 +0200

    feat: Add github link and copyright

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -3,10 +3,22 @@ import styles from "./Footer.module.css";
 
 const Footer: React.FC = () => {
     return (
-        /* Content of the footer goes here */
+        /* Footer container */
         < div className={styles.footerContainer} id="footer" >
             <div className={styles.footerWrapper}>
-
+                {/* Copyright an my github links */}
+                <p>
+                    © dvlp.energy 2024
+                    <a
+                        href="https://github.com/samuelandersoncodes"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        {" "}
+                        <br />
+                        Samuel Anderson's GitHub{" "}
+                    </a>
+                </p>
             </div>
         </div >
     );
