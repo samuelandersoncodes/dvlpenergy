730d452050b738cbaaf3cf29d591c62addbee312
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 13:13:39 +0200

    test: Set up base object for test cases

--- brandenburg_plants/test_views.py
+++ brandenburg_plants/test_views.py
@@ -6,3 +6,18 @@ from .models import SolarPlant
 apiUrlEndpoint = "http://127.0.0.1:8000/api/solar_plants/"
 
 
+class SolarPlantViewSetTestCase(TestCase):
+    def setUp(self):
+        """
+        Set up a test environment by creating a sample SolarPlant object.
+        """
+        self.client = Client()
+        self.solar_plant_data = {
+            "name": "Test Solar Plant",
+            "geometry": '{"type": "Point", "coordinates": [1.0, 2.0]}',
+        }
+        self.solar_plant = SolarPlant.objects.create(**self.solar_plant_data)
+
+
+if __name__ == "__main__":
+    unittest.main()
