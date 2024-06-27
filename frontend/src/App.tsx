0ee73626f02bd87e8c50f8f9aefe24e1780b6ac5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 17:34:42 +0200

    Add destination route path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import SolarPlantMap from "./components/solarplantsmap/SolarPlantsMap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Layout from "./components/layout/layout";
 
 function App() {
 
   return (
-    <SolarPlantMap />
-  )
-  
+    <>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/destination" element={<SolarPlantMap />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </>
+  );
+
 }
 
 export default App;
