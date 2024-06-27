7e6f15a64f7a2bcf4037ca5bc86ade1be7f75925
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 13:25:56 +0200

    test: Add test case for api mocking response

--- brandenburg_plants/test_views.py
+++ brandenburg_plants/test_views.py
@@ -9,7 +9,8 @@ apiUrlEndpoint = "http://127.0.0.1:8000/api/solar_plants/"
 class SolarPlantViewSetTestCase(TestCase):
     def setUp(self):
         """
-        Set up a test environment by creating a sample SolarPlant object.
+        Set up a test environment by creating a
+        sample SolarPlant object.
         """
         self.client = Client()
         self.solar_plant_data = {
@@ -29,12 +30,27 @@ class SolarPlantViewSetTestCase(TestCase):
 
     def test_retrieve_solar_plant(self):
         """
-        Test retrieving a specific solar plant by its ID via GET 
-        request to the API endpoint.
+        Test retrieving a specific solar plant by its
+        ID via GET request to the API endpoint.
         """
         response = self.client.get(f"{apiUrlEndpoint}{self.solar_plant.id}/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()["name"], self.solar_plant_data["name"])
+
+    def test_create_solar_plant(self):
+        """
+        Test creating a new solar plant via POST
+        request to the API endpoint.
+        """
+        new_solar_plant_data = {
+            "name": "New Test Solar Plant",
+            "geometry": '{"type": "Point", "coordinates": [2.0, 3.0]}',
+        }
+        response = self.client.post(
+            apiUrlEndpoint, new_solar_plant_data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(SolarPlant.objects.count(), 2)
 
 
 if __name__ == "__main__":
