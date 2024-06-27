aee40431573ec46277988f0438f5ba1290749021
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 16:55:24 +0200

    feat: Add layout props

--- frontend/src/components/layout/layout.tsx
+++ frontend/src/components/layout/layout.tsx
@@ -1,7 +1,12 @@
+import React, { ReactNode } from 'react';
 import Header from "../header/Header";
 
+interface LayoutProps {
+    children: ReactNode;
+}
 
-const Layout: React.FC = ({ children }) => {
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div>
             <Header />
