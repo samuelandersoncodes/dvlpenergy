8a065aa566848dcece1b111892ef90ebc40da6bf
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 17:14:47 +0200

    feat: Add home core structure

---
+++ frontend/src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+import styles from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Home;
