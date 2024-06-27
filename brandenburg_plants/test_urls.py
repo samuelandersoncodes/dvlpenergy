82ace53363fbde126e245c7717cca6d4bf53e545
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 13:41:27 +0200

    test: Set up initial data and client test

--- brandenburg_plants/test_urls.py
+++ brandenburg_plants/test_urls.py
@@ -4,3 +4,26 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from .models import SolarPlant
+
+class SolarPlantViewSetTestCase(TestCase):
+    """
+    Test case for SolarPlantViewSet API endpoints.
+    """
+
+    def setUp(self):
+        """
+        Set up initial data and client for testing.
+        """
+        self.client = APIClient()
+        self.url = reverse('solarplant-list')
+
+        # Create a sample SolarPlant object for testing
+        self.solar_plant_data = {
+            'name': 'Test Solar Plant',
+            'geometry': '{"type": "Point", "coordinates": [1.0, 2.0]}'
+        }
+        self.solar_plant = SolarPlant.objects.create(**self.solar_plant_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
