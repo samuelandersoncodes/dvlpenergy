d3a0229ef5ca211549e8ff59f94ec09f1b8a45fa
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 13:12:11 +0200

    test: Define api endpoint for test cases

--- brandenburg_plants/test_views.py
+++ brandenburg_plants/test_views.py
@@ -2,3 +2,7 @@ import unittest
 from django.test import TestCase, Client
 from .models import SolarPlant
 
+# Define the API endpoint URL
+apiUrlEndpoint = "http://127.0.0.1:8000/api/solar_plants/"
+
+
