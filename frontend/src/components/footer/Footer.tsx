99e942eb084e1d7314feec9794c76214471eaa55
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 21:54:59 +0200

    feat: Add footer core structure

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer: React.FC = () => {
+    return (
+        /* Content of the footer goes here */
+        < div className={styles.footerContainer} id="footer" >
+            <div className={styles.footerWrapper}>
+
+            </div>
+        </div >
+    );
+};
+
+export default Footer;
