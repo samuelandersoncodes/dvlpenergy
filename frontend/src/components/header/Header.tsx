12e9f08f68cf138d69a4f22308fcd3846ea3c6c8
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 18:16:56 +0200

    feat: Add logo

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,9 +1,21 @@
+import logo from "/dvlp-logo-transparent.png";
+import styles from "./Header.module.css"
+
 const Header: React.FC = () => {
 
     return (
-        <></>
+        <div className={styles.headerContainer}>
+            <div className={styles.headerWrapper}>
+                {/* logo */}
+                <div>
+                    <a href="/">
+                        <img src={logo} className={styles.logo} alt="company logo" />
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 
-}
+};
 
 export default Header;
