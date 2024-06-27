f4b66c9416fd36bf17c296450c1b427478f814b0
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-27 17:28:55 +0200

    feat: Add background video and open map button

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,11 +1,27 @@
 import styles from "./Home.module.css";
 import { useNavigate } from "react-router-dom";
+import video from "../../../public/home-video.mp4";
 
 const Home = () => {
-
+    // Initialize navigate function
+    const navigate = useNavigate();
+    // Function to navigate to '/destination' route
+    const handleOpenMap = () => {
+        navigate("/destination");
+    };
     return (
-        <div>
-
+        <div className={styles.container}>
+            {/* Container div for the home component */}
+            <div className={styles.videoContainer}>
+                {/* video background */}
+                <video autoPlay loop muted className={styles.videoBackground}>
+                    <source src={video} type="video/mp4" />
+                </video>
+            </div>
+            {/* Button to trigger navigation to the map */}
+            <button className={styles.btn} onClick={handleOpenMap}>
+                Open Map
+            </button>
         </div>
     );
 };
