ab6693f1dbbdf0c195a0897b95d3d75945a54b77
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-28 09:38:45 +0200

    refactor: Make copyright year dynamic

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer: React.FC = () => {
+
+    // Get current copyright year dynamically
+    const copyRightYear = new Date().getFullYear();
+
     return (
         /* Footer container */
         < div className={styles.footerContainer} id="footer" >
             <div className={styles.footerWrapper}>
                 {/* Copyright an my github links */}
                 <p>
-                    © dvlp.energy 2024
+                    © dvlp.energy {copyRightYear}
                     <a
                         href="https://github.com/samuelandersoncodes/dvlpenergy-task"
                         target="_blank"
