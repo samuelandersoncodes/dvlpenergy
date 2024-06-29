67446010b75676ffc18da81721c3d1adff64c56e
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-06-29 13:26:41 +0200

    fix: Modify urls to include frontend paths

--- solar_farm_map/urls.py
+++ solar_farm_map/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("brandenburg_plants.urls")),
+    re_path(r'^(?!admin|api).*$', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
